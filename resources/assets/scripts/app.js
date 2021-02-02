ab6ca2d87a20e8d9908d349ab686e9329fb5649f
sokmean-mptc <ngonsokmean@mptc.gov.kh>
2021-02-02 13:48:23 +0700

    #

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -5,6 +5,8 @@
 // import 'bootstrap';
 
 import 'provincial-v2/dist/main.js'
+import 'typeahead.js/dist/typeahead.bundle.js'
+import Bloodhound from 'typeahead.js/dist/bloodhound.js'
 
 jQuery('.map-collapse').on( 'click', function(){
   jQuery(this).children().toggleClass('icofont-plus icofont-minus')
@@ -56,3 +58,30 @@ function App() {
 }
 if ( document.getElementById("gallery") )
 ReactDOM.render(<App />, document.getElementById("gallery"));
+
+const Handlebars = require("handlebars")
+jQuery('.search-typeahead').each( function(){
+  let data = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: this.dataset.apiurl,
+  });
+  jQuery(this).typeahead( null, {
+    name: 'service',
+    displayKey:'title',
+    source: data.ttAdapter(),
+    templates: {
+      empty: [
+        '<div class="empty-message">',
+        'Not Found',
+        '</div>'
+      ].join('\n'),
+        suggestion: Handlebars.compile('<p><a href="{{url}}">{{title}}</a></p>')
+    }
+  } )
+
+  jQuery(this).bind('typeahead:select', function(ev, suggestion) {
+    window.location = suggestion.url
+  })
+
+})
